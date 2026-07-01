2b425106ef2d272e8f6ab64e3f22c0e8073e7115
Dave Hoffman <dve.hffman@gmail.com>
2026-06-30 22:20:08 -0400

    redirect skymap

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './style.css';
 
-createRoot(document.getElementById('root')).render(<App />);
+const redirect = sessionStorage.redirect;
+
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, '', redirect);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
